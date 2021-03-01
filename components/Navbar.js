92e8559a872b254b2771933e5c5be613b2d1f525
jeswanthv <jeswanthv001@gmail.com>
2021-03-01 21:37:14 +0530

    v:0.0.4

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,13 +5,13 @@ import React from 'react';
 const Navbar = () => {
   return (
     <div className='flex justify-between items-center h-10 '>
-      <h1 className='m-4 text-lg text-pink-600'>Daily Capital</h1>
+      <h1 className='m-4 text-xl text-pink-600'>Daily Capital</h1>
       <div className=''>
         <Link href='/'>
-          <a className='m-4 text-sm text-gray-100'>Home</a>
+          <a className='m-4 text-lg text-gray-100'>Home</a>
         </Link>
         <Link href='/feed/1'>
-          <a className='m-4 text-sm text-gray-100'>Feed</a>
+          <a className='m-4 text-lg text-gray-100'>Feed</a>
         </Link>
       </div>
     </div>
