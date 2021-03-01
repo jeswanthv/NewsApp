b1ba169cb8b98ce2a682057d7dc9e58a2ffc9e6f
jeswanthv <jeswanthv001@gmail.com>
2021-03-01 21:27:59 +0530

    v:0.0.2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: ['./pages/**/*.js', './styles/**/*.css'],
+  purge: ['./pages/**/*.js', './styles/**/*.css', './components/*.js'],
   future: {
     removeDeprecatedGapUtilities: true,
     purgeLayerByDefault: true,
